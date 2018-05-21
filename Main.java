598c0fa4dbc812147aed3fe6d51dd0471240b693
Elliot <e99pickens@gmail.com>
2018-05-20 22:19:24 -0500

    Tester

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double test = 2.76;
+        double approx = ContinuedFractions.contFrac(0.333, 10);
+        System.out.println(approx);
+    }
+}
